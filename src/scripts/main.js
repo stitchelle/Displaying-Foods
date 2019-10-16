77bdc4e2fdda49e05c9f79aab144d2b9050173bd
Michelle Johnson <bob1234girl@gmail.com>
2019-10-16 16:13:57 -0500

    fixed git commit mix up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,35 @@
 /* 
 Step 2: Create a function which returns a string template. The template is the HTML representation for a food item.
 */
-fetch("http://localhost:8088/food")
-    .then(foods => foods.json())
-    .then(parsedFoods => {
-        parsedFoods.forEach(food => {
-            const foodAsHTML = foodFactory(food)
-            addFoodToDom(foodAsHTML)
-        })
-    })
 
 
+
+let foodClass = ""
+const createFoodComponent = (food) =>{
+    return `
+        <div class = "foodItem">
+            <h1>${food.id}</h1>
+            <section>${food.name}</section>
+            <section>${food.type}</section>
+            <section>${food.ethnicity}</section>
+        </div>
+    `
+}
 
 /*
 Step 3: Create a function that inserts an HTML representation of a food into the DOM
 */
+
+
+// fetch call from exercise
+
+fetch("http://localhost:8088/food")
+    .then(foods => foods.json())
+    .then(parsedFoods => {
+        parsedFoods.forEach(food => {
+            const foodHTML = createFoodComponent(food)
+            const foodContainer = document.querySelector(".foodList");
+            foodContainer.innerHTML += foodHTML;
+            //put in a function: two lines above this
+        })
+    })
